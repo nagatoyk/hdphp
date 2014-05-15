50b36c654a2dde99b385ce5159d9ab7b3f96da74
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-05-16 00:54:46 +0800

    更新版本号

--- hdphp/hdphp.php
+++ hdphp/hdphp.php
@@ -16,7 +16,7 @@
  * @supackage core
  * @author hdxj <houdunwangxj@gmail.com>
  */
-define('HDPHP_VERSION', '2014-05-15');
+define('HDPHP_VERSION', '2014-05-16');
 defined("DEBUG")        or define("DEBUG", FALSE);
 if (!defined('GROUP_PATH'))
     defined('APP_PATH') or define('APP_PATH', './');
